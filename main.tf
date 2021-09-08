6fa8dad9c07ad6f4613e9ff9378711cb531737a1
crazy4devops <crazy4devops@gmail.com>
2021-09-08 11:23:53 +0530

    updated the code

--- main.tf
+++ main.tf
@@ -1,32 +1,19 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# DEPLOY A SINGLE EC2 INSTANCE
-# This template runs a simple "Hello, World" web server on a single EC2 Instance
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# ----------------------------------------------------------------------------------------------------------------------
-# REQUIRE A SPECIFIC TERRAFORM VERSION OR HIGHER
-# ----------------------------------------------------------------------------------------------------------------------
-
 terraform {
-  # This module is now only being tested with Terraform 0.15.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.15.x code.
-  required_version = ">= 0.12.26"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
 }
-
-# ------------------------------------------------------------------------------
-# CONFIGURE OUR AWS CONNECTION
-# ------------------------------------------------------------------------------
 
 provider "aws" {
-  region = "us-east-2"
+  profile = "default"
+  region  = "us-east-2"
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# DEPLOY A SINGLE EC2 INSTANCE
-# ---------------------------------------------------------------------------------------------------------------------
-
-resource "aws_instance" "example" {
+resource "aws_instance" "webserver" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
@@ -39,16 +26,11 @@ resource "aws_instance" "example" {
               EOF
 
   tags = {
-    Name = "terraform-example"
+    Name = "sample-webserver"
   }
 }
-
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE THE SECURITY GROUP THAT'S APPLIED TO THE EC2 INSTANCE
-# ---------------------------------------------------------------------------------------------------------------------
-
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = "sample-webserver-instance"
 
   # Inbound HTTP from anywhere
   ingress {
