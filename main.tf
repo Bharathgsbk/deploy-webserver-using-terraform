24b13c8e5b525ec88d406108f760844d2643646e
crazy4devops <crazy4devops@gmail.com>
2021-09-08 11:33:07 +0530

    updated the code

--- main.tf
+++ main.tf
@@ -12,7 +12,6 @@ provider "aws" {
   profile = "default"
   region  = "us-east-2"
 }
-
 resource "aws_instance" "webserver" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
   ami                    = "ami-0c55b159cbfafe1f0"
@@ -21,7 +20,7 @@ resource "aws_instance" "webserver" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, World\n sample webserver" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
