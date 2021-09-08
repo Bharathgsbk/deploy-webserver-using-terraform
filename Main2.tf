4b79218020db3ef4e5c39cb5ba4a3bf37a891bf2
Bharathgsbk <35695321+Bharathgsbk@users.noreply.github.com>
2021-09-08 23:41:53 +0530

    Create Main2.tf

---
+++ Main2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "my-instance" {
+  count         = var.instance_count
+  ami           = lookup(var.ami,var.aws_region)
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.terraform-demo.key_name
+  user_data     = file("install_apache.sh")
+
+  tags = {
+    Name  = "Terraform-${count.index + 1}"
+    Batch = "5AM"
+  }
+}
