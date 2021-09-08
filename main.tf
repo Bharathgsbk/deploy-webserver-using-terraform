407e8ffb11cf92d9afbebe83b6a3c5935f8bb948
Bharathgsbk <35695321+Bharathgsbk@users.noreply.github.com>
2021-09-08 22:52:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,22 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_lb" "test" {
+  name               = "lb-tf"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = aws_subnet.public.*.id
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
